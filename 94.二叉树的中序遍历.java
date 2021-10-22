b990cd20dbd84e722177dc208518ec7ff367364e
terloo <chensijie08@gmail.com>
2021-10-22 21:59:24 +0800

    100%

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,120 @@
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.57%)
+ * Likes:    1140
+ * Dislikes: 0
+ * Total Accepted:    583.1K
+ * Total Submissions: 771.6K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        List<TreeNode> nodes = new ArrayList<>();
+        nodes.add(root);
+
+        TreeNode tmp;
+        while (nodes.size() != 0) {
+            tmp = nodes.get(nodes.size() - 1);
+            if (tmp.left != null) {
+                nodes.add(tmp.left);
+                tmp.left = null;
+                continue;
+            }
+            nodes.remove(nodes.size() - 1);
+            result.add(tmp.val);
+            if (tmp.right != null) {
+                nodes.add(tmp.right);
+                tmp.right = null;
+                continue;
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
