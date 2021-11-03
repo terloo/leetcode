db596f14ba38bb7dc24a87a1d06ef5d0fd89d8d4
terloo <chensijie08@gmail.com>
2021-11-03 22:59:45 +0800

    5.26%

---
+++ 22.括号生成.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=22 lang=java
+ *
+ * [22] 括号生成
+ *
+ * https://leetcode-cn.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (77.27%)
+ * Likes:    2132
+ * Dislikes: 0
+ * Total Accepted:    369.7K
+ * Total Submissions: 478.4K
+ * Testcase Example:  '3'
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * 
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：["()"]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        num = n;
+        back(n * 2);
+
+        return results;
+    }
+
+    String lBrackets = "(";
+    String rBrackets = ")";
+
+    private int num;
+
+    private List<String> results = new ArrayList<>();
+    private List<String> result = new ArrayList<>();
+
+    private void back(int n) {
+        if (!valid()) {
+            return;
+        }
+        if (n == 0) {
+            if (Collections.frequency(result, "(") == num && Collections.frequency(result, ")") == num) {
+                results.add(String.join("", result));
+            }
+            return;
+        }
+
+        result.add(lBrackets);
+        back(n - 1);
+        result.remove(result.size() - 1);
+        result.add(rBrackets);
+        back(n - 1);
+        result.remove(result.size() - 1);
+    }
+
+    private boolean valid() {
+        if (result.size() == 0) {
+            return true;
+        }
+        LinkedList<String> stack = new LinkedList<>();
+        for (int i = 0; i < result.size(); i++) {
+            if (lBrackets.equals(result.get(i))) {
+                stack.push(lBrackets);
+                if (stack.size() > num) {
+                    return false;
+                }
+            } else {
+                if (stack.poll() == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
