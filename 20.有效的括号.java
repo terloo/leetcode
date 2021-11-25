ced90205cff694742324a858f8be52d62e193ec6
terloo <chensijie08@gmail.com>
2021-11-25 23:08:29 +0800

    98.01%

---
+++ 20.有效的括号.java
@@ -0,0 +1,100 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (44.53%)
+ * Likes:    2784
+ * Dislikes: 0
+ * Total Accepted:    843.7K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "()"
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = "(]"
+ * 输出：false
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：s = "([)]"
+ * 输出：false
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：s = "{[]}"
+ * 输出：true
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * s 仅由括号 '()[]{}' 组成
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                char popC = stack.pop();
+                if ( (popC == '(' && c != ')')
+                    || (popC == '[' && c != ']')
+                    || (popC == '{' && c != '}')) {
+                    return false;
+                }
+            }
+
+        }
+
+        return stack.size() == 0;
+    }
+}
+// @lc code=end
